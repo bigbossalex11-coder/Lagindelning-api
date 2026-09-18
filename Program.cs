d9ff6d5fd600c0570e7fc6ed03e160267a34186d
Alex <bigbossalex11@gmail.com>
2026-09-18 10:03:51 +0200

    tillåt CORS från react-appen

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+     options.AddDefaultPolicy(policy =>
+     policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,7 +24,7 @@ var players = new List<Player>
     new Player(2, "eva" ,"gul"),
     new Player(3, "oskar" ,"röd")
 };
-
+app.UseCors();
 app.MapGet("/players", () => players);
 
 app.Run();
